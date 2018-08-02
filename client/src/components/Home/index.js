ab5b7fd95e58bc37b6fd6006a398dff4611222e5
Dzmitry Aleinik <dzmitry.aleinik@itechart-group.com>
2018-08-02 20:01:55 +0300

    Sign in and Sign up form (not finished)

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+    
+    render() {
+        return(
+            <form>
+                <input>
+                </input>
+                <input>
+                </input>
+                <button>Ok</button>
+            </form>
+
+        )
+    }
+}
+
+export default Home;
+
+
---
+++ client/src/components/SignIn/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class SignIn extends React.Component {
+    constructor() {
+
+    }
+    
+    render() {
+        return(
+            <form>
+                <input>
+                </input>
+                <input>
+                </input>
+                <button>Ok</button>
+            </form>
+
+        )
+    }
+}
+
+export default SignIn;
+
+
