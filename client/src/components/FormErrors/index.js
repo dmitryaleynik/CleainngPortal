17f5a561de4b563ea9b4e8ff41c295c8fcffd164
Dzmitry Aleinik <dzmitry.aleinik@itechart-group.com>
2018-08-09 12:40:38 +0300

    ErrorForm component

---
+++ client/src/components/FormErrors/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+ const FormErrors = ({formErrors}) =>
+  <div className='formErrors'>
+    {Object.keys(formErrors).map((fieldName, i) => {
+      if(formErrors[fieldName].length > 0){
+        return (
+          <p key={i}>{fieldName} {formErrors[fieldName]}</p>
+        )        
+      } else {
+        return '';
+      }
+    })}
+  </div>
+  export default FormErrors;
