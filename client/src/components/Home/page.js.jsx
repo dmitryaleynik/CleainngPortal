1b6b8e6b6541b14002a4863da5effb95c32c80fd
Aleinik Dmitry <gerdalex39@gmail.com>
2018-10-21 23:37:06 +0300

    Added finding of the key length

--- client/src/components/Home/page.js.jsx
+++ client/src/components/Home/page.js.jsx
@@ -5,9 +5,9 @@ import {encrypt, decrypt} from '../../actions/securityActions';
 class Home extends React.Component {
   render() {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const text = 'ATTACKATDAWN';
-    const key = 'LEMONLEMONLE';
-
+    const text = `THEREARETWOWAYSOFCONSTRUCTINGASOFTWAREDESIGNONEWAYISTOMAKEITSOSIMPLETHATTHEREAREOBVIOUSLYNODEFICIENCIESANDTHEOTHERWAYISTOMAKEITSOCOMPLICATEDTHATTHEREARENOOBVIOUSDEFICIENCIESTHEFIRSTMETHODISFARMOREDIFFICULT`;
+    const key = 'SYSTEM';
+    const rightCipher = `LFWKIMJCLPSISWKHJOGLKMVGURAGKMKMXMAMJCVXWUYLGGIISWALXAEYCXMFKMKBQBDCLAEFLFWKIMJCGUZUGSKECZGBWYMOACFVMQKYFWXTWMLAIDOYQBWFGKSDIULQGVSYHJAVEFWBLAEFLFWKIMJCFHSNNGGNWPWDAVMQFAAXWFZCXBVELKWMLAVGKYEDEMJXHUXDAVYXL`
     const encryptedText = this.encrypt(
       alphabet,
       text,
@@ -18,6 +18,12 @@ class Home extends React.Component {
       encryptedText,
       key, 
     )
+    const repetitions = this.findRepetitions(encryptedText);
+    const filteredRepetitions = this.filterRepetitions(repetitions);
+    const shifts = this.findShifts(filteredRepetitions);
+    const shiftsCount = this.getShiftCount(shifts);
+    const keyLength = this.geKeyLength(shiftsCount);
+    debugger;
   return (
     <div>
       <h1>
@@ -42,7 +48,6 @@ class Home extends React.Component {
       const encryptedChar = alphabet[alphPositionOfEncryptedChar]
       encryptedText += encryptedChar;
     }); 
-    alert(encryptedText);
 
     return encryptedText;
   }
@@ -63,11 +68,96 @@ class Home extends React.Component {
       decryptedText += decryptedChar; 
     });
 
-    alert(decryptedText);
-
     return decryptedText;
-
   }
+
+  findRepetitions(text) {
+    debugger;
+    let repetitions = {};
+    for(let i = 3; i < 7; i++ ) {
+      let lengthOfRepetition = i;
+      for (let j = 0; j < text.length - lengthOfRepetition; j++) {
+        let searchString = text.substring(j, j + lengthOfRepetition);
+        if (repetitions[searchString]) {
+          continue;
+        }
+        for (let k = j + lengthOfRepetition; k < text.length; k++) {
+          let checkedForRepetitionString = text.substring(k, k + lengthOfRepetition);
+            if (checkedForRepetitionString === searchString) {
+              this.setRepetition(repetitions, searchString, j, k);    
+            }  
+        }
+      }  
+    }
+    return repetitions;  
+  }
+
+  setRepetition(repetitions, str, indexOfFirstOccurence, indexOfOccurence) {
+    if (!repetitions[str]) {
+      repetitions[str] = [];
+      repetitions[str].push(indexOfFirstOccurence);
+    }
+    repetitions[str].push(indexOfOccurence);
+  }
+
+  findShifts(repetitions) {
+    let shifts = {};
+    for (let key in repetitions) {
+      shifts[key] = this.getDifferences(repetitions[key]);
+    }
+    return shifts;
+  }
+
+  getDifferences(array) {
+    let differences = [];
+    for (let i = 0; i < array.length - 1; i++) {
+      differences.push(array[i + 1] - array[i]);
+    } 
+    return differences;
+  }
+
+  getShiftCount(shifts) {
+    let shiftCounter = {};
+    for (let key in shifts) {
+      shifts[key].forEach((shift) => {
+        if (!shiftCounter[shift]) {
+          shiftCounter[shift] = 0;
+        }
+        shiftCounter[shift]++;    
+      });  
+    }
+    return shiftCounter;
+  }
+
+  geKeyLength(shiftCounter) {
+    const keyLength = Object.keys(shiftCounter).reduce((prevValue, currentValue) => this.getGCD(prevValue, currentValue));
+    return keyLength;
+  }
+
+  getGCD (first, second) {
+    if (!second) {
+        return first;
+    }
+    return this.getGCD(second, first % second);
+  }
+
+
+  filterRepetitions (repetitions) {
+    let filteredRepetitions = {};
+    for (let key in repetitions) {
+      if (repetitions[key].length > 2) {
+        filteredRepetitions[key] = repetitions[key];
+      }
+    }
+    return filteredRepetitions;
+  }
+
+
+
+  
+    
+
+
 }
 
 export default Home;
